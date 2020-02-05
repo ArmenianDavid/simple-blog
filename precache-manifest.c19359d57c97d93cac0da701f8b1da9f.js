ccb49a88092c135131ceafbb521e6fe7b84def98
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-05 18:23:55 +0400

    Updates

---
+++ precache-manifest.c19359d57c97d93cac0da701f8b1da9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309b15f675e720036c98a0633141aa78",
+    "url": "/simple-blog/index.html"
+  },
+  {
+    "revision": "58bb3f7477c3569e5218",
+    "url": "/simple-blog/static/css/main.b4540cb8.chunk.css"
+  },
+  {
+    "revision": "aecb2d143b86492fa582",
+    "url": "/simple-blog/static/js/2.fa3d789c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-blog/static/js/2.fa3d789c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bb3f7477c3569e5218",
+    "url": "/simple-blog/static/js/main.7f18bda3.chunk.js"
+  },
+  {
+    "revision": "46211089dd83fdbda855",
+    "url": "/simple-blog/static/js/runtime-main.d5f488c5.js"
+  }
+]);
